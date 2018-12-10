e9dbb7ed5205129bc1256d45e43735f1887c6d42
whjin <1414848321@qq.com>
2018-12-11 00:49:09 +0800

    push code

--- test.php
+++ test.php
@@ -6,25 +6,27 @@
  * Time: 13:51
  */
 $con = mysqli_connect('localhost', 'root', 'caozhigy359', 'info');
-mysqli_set_charset($con, "utf8");
-if ($con) {
+//mysqli_set_charset($con, "utf8");
+mysqli_query($con, 'set names utf8');
+/*if ($con) {
     echo '连接数据库成功';
 } else {
     echo '连接数据库失败';
 }
-echo '<br>';
-$insert = mysqli_query($con, "INSERT INTO test(name) VALUES ('毛泽东')");
+echo '<br>';*/
+/*$insert = mysqli_query($con, "INSERT INTO test(name) VALUES ('邓小平')");
 var_dump($insert);
 if ($insert) {
     echo '插入数据成功';
 } else {
     echo '插入数据失败';
 }
-echo '<br>';
+echo '<br>';*/
 $res = mysqli_query($con, "SELECT * FROM test");
+print_r(mysqli_fetch_row($res));
 $row = mysqli_fetch_all($res);
 var_dump($row);
-echo '<br>';
+/*echo '<br>';
 mysqli_close($con);
-echo '数据库已经关闭';
+echo '数据库已经关闭';*/
 
