9b41872cb7c1a772556ecd507deaa074093b364d
whjin <1414848321@qq.com>
2018-12-11 15:40:54 +0800

    push code

--- article/config.php
+++ article/config.php
@@ -6,3 +6,6 @@
  * Time: 2:09
  */
 //配置文件
+define('HOST', '127.0.0.1');
+define('USERNAME', 'root');
+define('PASSWORD', 'caozhigy359');
--- article/connect.php
+++ article/connect.php
@@ -6,3 +6,11 @@
  * Time: 2:10
  */
 //MYSQL连接初始化程序
+require_once 'config.php';
+$con = mysqli_connect(HOST, USERNAME, PASSWORD, 'info');
+mysqli_query($con, 'set names utf8');
+if ($con) {
+    echo '连接数据库成功';
+} else {
+    echo '连接数据库失败';
+}
